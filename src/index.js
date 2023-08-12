cce3b413093c22714882c830a11289cd7e500290
ttaerrim <ltr0121@naver.com>
2023-08-12 16:24:17 +0900

    modify: make route module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 
+const api = require("./api");
+
 const app = new Koa();
 const router = new Router();
 
-// 첫 번째는 path, 두 번째는 라우트 처리할 함수 전달
-router.get("/", (ctx) => {
-  ctx.body = "home";
-});
-router.get("/about/:name?", (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : "소개";
-});
+router.use("/api", api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
